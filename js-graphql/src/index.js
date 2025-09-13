4d9ecad34633d3786b81364dcf408b6fff77abbb
isaackhlam <isaac.lamkh@gmail.com>
2025-09-14 01:25:20 +0800

    feat: convert server from node:http to aws lambda

--- js-graphql/src/index.js
+++ js-graphql/src/index.js
@@ -1,25 +1,69 @@
 import { createSchema, createYoga } from 'graphql-yoga';
-import { createServer } from 'http';
+import { pipeline } from 'stream/promises'
 
 import typeDefs from './schemas/index.js';
 import resolvers from './resolvers/index.js';
 
-const main = async () => {
-  const yoga = createYoga({
-    schema: createSchema({
-      typeDefs: await typeDefs,
-      resolvers,
-    }),
-    cors: {
-      origin: '*',
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: await typeDefs,
+    resolvers,
+  }),
+  graphqlEndpoint: '/',
+  cors: {
+    origin: '*',
+  },
+});
+
+const classicHandler = async (event, context) => {
+  const response = await yoga.fetch(
+    `http://${event.headers.host}${event.rawPath}${event.rawQueryString ? '?' + event.rawQueryString : ''}`,
+    {
+      method: event.requestContext.http?.method ?? event.httpMethod,
+      headers: event.headers,
+      body: event.isBase64Encoded ? Buffer.from(event.body, 'base64') : event.body
     },
-  });
+    { event, context }
+  )
 
-  const server = createServer(yoga);
-  server.listen(4000, () => {
-    // eslint-disable-next-line no-console
-    console.info('Server is running on http://localhost:4000/graphql');
-  });
-};
+  return {
+    statusCode: response.status,
+    headers: Object.fromEntries(response.headers.entries()),
+    body: await response.text()
+  }
+}
 
-main();
+const streamingHandler = awslambda.streamifyResponse(async function handler(
+  event, res, lambdaContext) {
+  const path = event.requestContext.http?.path ?? event.path
+  const method = event.requestContext.http?.method ?? event.httpMethod
+  const domainName = event.requestContext.domainName
+  const query = event.rawQueryString ?? new URLSearchParams(event.queryStringParameters || {}).toString()
+
+  const response = await yoga.fetch(
+    `https://${domainName}${path}${query ? `?{query}` : ''}`,
+    {
+      method,
+      headers: event.headers,
+      body: event.body && event.isBase64Encoded ? Buffer.from(event.body, 'base64') : event.body
+    },
+    {
+      event,
+      lambdaContext,
+      res
+    }
+  )
+  res = awslambda.HttpResponseStream.from(res, {
+    statusCode: response.status,
+    headers: Object.fromEntries(response.headers.entries())
+  })
+
+  if (response.body) {
+    await pipeline(response.body, res)
+  }
+
+  res.end()
+})
+
+const isLocal= !!process.env.AWS_SAM_LOCAL
+export const handler = isLocal ? classicHandler : streamingHandler
