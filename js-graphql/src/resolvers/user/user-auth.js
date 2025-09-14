48b6ca0060ace9d8c36db2beff841d099467a036
isaackhlam <isaac.lamkh@gmail.com>
2025-09-15 03:30:54 +0800

    feat: login function with jwt

--- js-graphql/src/resolvers/user/user-auth.js
+++ js-graphql/src/resolvers/user/user-auth.js
@@ -4,18 +4,23 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 import { v4 as uuidv4 } from 'uuid';
 import { DynamoDBClient, PutItemCommand, GetItemCommand } from '@aws-sdk/client-dynamodb';
+import { getUserByUsername } from './user-impl.js';
 
 const { USER_ID_TABLE_NAME } = process.env;
 //TODO: No hardcode region
 const dynamoDBClient = new DynamoDBClient({region: "ap-east-2"});
 
-const userRegister = async (_p, { input }) => {
+//TODO: No hardcode and exposed secret...
+const createToken = async ({ id }) => jwt.sign({ id }, 'abc', { expiresIn: '1d' });
+
+const registerUser = async (_p, { input }) => {
   console.log(input);
   const { username, email, password } = input;
   //TODO: check existing user before register.
 
   const id = uuidv4();
   const hashedPassword = await bcrypt.hash(password, Number(2));
+  //TODO: Use Transact to prevent race condition
   const command = new PutItemCommand({
     TableName: USER_ID_TABLE_NAME,
     Item: {
@@ -38,5 +43,19 @@ const userRegister = async (_p, { input }) => {
   return { id, username };
 }
 
+const loginUser = async (_p, { input }) => {
+  const { username, password }  = input;
+  console.log("username:", username, " password: ", password);
+  const user = await getUserByUsername(username);
+  console.log("user:", user)
 
-export { userRegister };
+  if (!user) throw Error('User Not Found!');
+
+  const hashedPassword = user.Item.password.S;
+  const isPasswordValid = await bcrypt.compare(password, hashedPassword);
+  if (!isPasswordValid) throw Error('Wrong Password!');
+
+  return { token: await createToken({ id: user.Item.id.S }) };
+}
+
+export { registerUser, loginUser };
---
+++ js-graphql/src/resolvers/user/user-impl.js
@@ -0,0 +1,34 @@
+import { DynamoDBClient, QueryCommand, GetItemCommand } from '@aws-sdk/client-dynamodb';
+
+const { USER_ID_TABLE_NAME, USER_ID_TABLE_USERNAME_GSI_NAME } = process.env;
+//TODO: No hardcode region
+const dynamoDBClient = new DynamoDBClient({region: "ap-east-2"});
+
+const getUserByUsername = async (username) => {
+  const command = new QueryCommand({
+    TableName: USER_ID_TABLE_NAME,
+    IndexName: USER_ID_TABLE_USERNAME_GSI_NAME,
+    KeyConditionExpression: "username = :username",
+    ExpressionAttributeValues: {
+      ":username": {"S": username}
+    }
+  });
+
+  const result = await dynamoDBClient.send(command);
+  return result.Count === 1 ? {"Item": result.Items[0]} : null;
+}
+
+
+const getUserById = async (id) => {
+  const command = new GetItemCommand({
+    TableName: USER_TABLE,
+    Key: {
+      id: {
+        S: id,
+      },
+    },
+  });
+  return await dynamoDBClient.send(command);
+};
+
+export { getUserById, getUserByUsername };
