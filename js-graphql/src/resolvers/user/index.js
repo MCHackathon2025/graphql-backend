f609eda605c8020c300bc3ae1794b65786838e88
isaackhlam <isaac.lamkh@gmail.com>
2025-09-15 03:51:55 +0800

    feat: add user info to context after login

--- js-graphql/src/resolvers/user/index.js
+++ js-graphql/src/resolvers/user/index.js
@@ -1,7 +1,11 @@
-import { registerUser, loginUser } from './user-auth.js';
+import { registerUser, loginUser, meResolver } from './user-auth.js';
 
 
 export const Mutation = {
   register: registerUser,
   login: loginUser
 };
+
+export const Query = {
+  me: meResolver,
+}
