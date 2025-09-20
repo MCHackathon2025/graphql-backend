c27a44424f3d0573f2dcfecab4296cacd1b06f36
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2025-09-20 19:50:13 +0800

    feat: resolver for all user events

---
+++ js-graphql/src/resolvers/calender/index.js
@@ -0,0 +1,5 @@
+import { createEvent } from './calender.js';
+
+export const Mutation = {
+  createEvent
+}
--- js-graphql/src/resolvers/index.js
+++ js-graphql/src/resolvers/index.js
@@ -3,7 +3,7 @@ import merge from 'lodash/merge.js';
 import * as healthcheck from './healthcheck.js';
 import * as user from './user/index.js';
 import * as weather from './weather.js';
-import * as calender from './calender.js';
+import * as calender from './calender/index.js';
 
 const resolvers = merge({}, ...[healthcheck, user, weather, calender]);
 
